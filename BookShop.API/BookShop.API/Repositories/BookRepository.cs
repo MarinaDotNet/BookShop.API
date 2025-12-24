8d7fc70bcfce03ab54cf3bd2ee018254af0fea6b
marinadotnet <msichova@outlook.com>
2025-12-24 11:32:24 +0800

    Add BookRepository
    feat: add BookRepository implementing IBookRepository
    
    -  Implements CRUD operations for the book collection
    - Provides a repository layer to access MongoDB collection through MongoDbContext

---
+++ BookShop.API/BookShop.API/Repositories/BookRepository.cs
@@ -0,0 +1,35 @@
+﻿using BookShop.API.Infrastructure.Persistence;
+using BookShop.API.Models;
+using MongoDB.Driver;
+using System.Runtime.CompilerServices;
+
+namespace BookShop.API.Repositories;
+
+/// <summary>
+/// Provides access to book data stored in a MongoDB database.
+/// </summary>
+/// <remarks>This repository implements data access operations for books using MongoDB. All methods are
+/// asynchronous and thread-safe. The repository should be disposed of when no longer needed to release database
+/// resources.</remarks>
+/// <param name="context">The MongoDB context used to access the book collection. Cannot be null.</param>
+public class BookRepository(MongoDbContext context) : IBookRepository
+{
+    /// <summary>
+    /// Represents the MongoDB collection used to store and retrieve <see cref="Book"/> entities.
+    /// </summary>
+    private readonly IMongoCollection<Book> _booksCollection = context.GetCollection();
+
+    /// <summary>
+    /// Asynchronously retrieves all books from the data store.
+    /// </summary>
+    /// <remarks>This method performs a read operation that returns all available books. The returned
+    /// collection reflects the current state of the data store at the time of the query. The order of books in the
+    /// collection is not guaranteed.</remarks>
+    /// <returns>A task that represents the asynchronous operation. The task result contains an enumerable collection of all <see
+    /// cref="Book"/> objects in the data store. If no books are found, the collection will be empty.</returns>
+    public async Task<IEnumerable<Book>> GetAllBooksAsync()
+    {
+        var books = await _booksCollection.Find(_ => true).ToListAsync();
+        return books;
+    }
+}
