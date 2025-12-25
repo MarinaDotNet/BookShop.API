6aa23c0507cc17625b4ed396ba3aa1dd65b9f403
marinadotnet <msichova@outlook.com>
2025-12-25 14:25:33 +0800

    Create BookMapingProfile
    feat: add BookMapingProfile
    
    - defines AutoMapper configuration
    - mapping between Book and BookDto objects

---
+++ BookShop.API/Mappings/BookMapingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BookShop.API.Models;
+
+namespace BookShop.API.Mappings;
+
+/// <summary>
+/// Defines AutoMapper configuration for mapping between Book and BookDto objects.
+/// </summary>
+/// <remarks>This profile is intended to be used with AutoMapper to facilitate object-to-object mapping in
+/// scenarios where Book entities need to be converted to BookDto data transfer objects. Register this profile with the
+/// AutoMapper configuration to enable the mapping.</remarks>
+public class BookMapingProfile : Profile
+{
+    public BookMapingProfile()
+    {
+        CreateMap<Book, BookDto>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id!.ToString()));
+            
+    }
+}
