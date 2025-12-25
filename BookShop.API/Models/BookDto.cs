f268ec385667071a6942b59e0720d901f30d9939
marinadotnet <msichova@outlook.com>
2025-12-25 14:23:56 +0800

    Create BookDto.cs
    feat: add record BookDto data transfer object

---
+++ BookShop.API/Models/BookDto.cs
@@ -0,0 +1,6 @@
+﻿namespace BookShop.API.Models;
+
+/// <summary>
+/// Data Transfer Object (DTO) representing a book.
+/// </summary>
+public sealed record BookDto(string Id, string Title, List<string> Authors, decimal Price, int Pages, string Publisher, string Language, List<string> Genres, Uri Link, bool IsAvailable, string Annotation);
