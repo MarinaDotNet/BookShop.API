4b68b9160272046c8d5ac64342cfddc0812c3bbd
marinadotnet <msichova@outlook.com>
2025-12-25 14:32:58 +0800

    Update IBookRepository.cs
    feat: add GetBookByIdAsync in IBookRepository

--- BookShop.API/Repositories/IBookRepository.cs
+++ BookShop.API/Repositories/IBookRepository.cs
@@ -13,4 +13,12 @@ public interface IBookRepository
     /// <returns>A task that represents the asynchronous operation. The task result contains a collection of all books. The
     /// collection is empty if no books are found.</returns>
     Task<IEnumerable<Book>> GetAllBooksAsync();
+
+    /// <summary>
+    /// Asynchronously retrieves a book with the specified identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the book to retrieve. Cannot be null or empty.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the <see cref="Book"/> with the
+    /// specified identifier, or <see langword="null"/> if no matching book is found.</returns>
+    Task<Book> GetBookByIdAsync(string id);
 }
