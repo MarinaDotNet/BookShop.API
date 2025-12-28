e533de588dd4c7a5ad328f2c19f79fb5a646ffc5
marinadotnet <msichova@outlook.com>
2025-12-29 00:02:08 +0800

    Create BookUpdateDto.cs
    feat: add record BookUpdateDto data transfer object

---
+++ BookShop.API/Models/BookUpdateDto.cs
@@ -0,0 +1,3 @@
+﻿namespace BookShop.API.Models;
+
+public sealed record BookUpdateDto(string Id, string? Title, List<string>? Authors, decimal? Price, int? Pages, string? Publisher, string? Language, List<string>? Genres, Uri? Link, bool? IsAvailable, string? Annotation);
