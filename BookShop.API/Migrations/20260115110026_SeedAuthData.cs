df144049b2261347620e0bb412d120a641c28f2f
marinadotnet <msichova@outlook.com>
2026-01-15 19:42:58 +0800

    Update database
    Add authorization seed data and add index on user role id

---
+++ BookShop.API/Migrations/20260115110026_SeedAuthData.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookShop.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedAuthData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEmailConfirmed",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "admin" },
+                    { 2, "user" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CreatedAt", "Email", "IsActive", "IsEmailConfirmed", "NormalizedEmail", "NormalizedUsername", "PasswordHash", "UpdatedAt", "UserName" },
+                values: new object[] { 1, new DateTime(2026, 1, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "admin@bookshop.api.com", true, true, "ADMIN@BOOKSHOP.API.COM", "ADMIN", "AQAAAAEAACcQAAAAEFWDfK8QnvlZsT6wjuSYyw2Xe4P1HTcaW5MavWsfHOaFY4CpvcPtbDZWP6XrT3Jkgg==", new DateTime(2026, 1, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "admin" });
+
+            migrationBuilder.InsertData(
+                table: "UserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { 1, 1 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRoles_UserId",
+                table: "UserRoles",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserRoles_UserId",
+                table: "UserRoles");
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "UserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DropColumn(
+                name: "IsEmailConfirmed",
+                table: "Users");
+        }
+    }
+}
