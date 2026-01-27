615a365ee8412b4f12adb32ee9b221b1f59472a4
marinadotnet <msichova@outlook.com>
2026-01-27 14:43:53 +0800

    Update
    Refactor AuthService and add email confirmation flow

--- BookShop.API/Services/AuthServices.cs
+++ BookShop.API/Services/AuthServices.cs
@@ -4,6 +4,7 @@ using BookShop.API.Repositories;
 using Microsoft.AspNetCore.Identity;
 using System.Text.RegularExpressions;
 using System.Threading;
+using static BookShop.API.Models.AuthTokens;
 
 namespace BookShop.API.Services;
 
@@ -19,10 +20,27 @@ namespace BookShop.API.Services;
 /// <param name="passwordHasher">
 /// The password hasher used to securely hash user passwords before storing them in the database.
 /// </param>
-public class AuthServices(IUserRepository userRepository, IPasswordHasher<User> passwordHasher)
+/// <param name="authLinkGenerator">
+/// The service responsible for generating authentication-related links, such as email confirmation links.
+/// </param>
+/// <param name="authTokenService">
+/// The service used to create and manage authentication tokens for various purposes, such as email confirmation.
+/// </param>
+/// <param name="emailSender">
+/// The service responsible for sending authentication-related emails, such as email confirmation messages.
+/// </param>
+public class AuthServices(
+    IUserRepository userRepository, 
+    IPasswordHasher<User> passwordHasher, 
+    IAuthTokenService authTokenService, 
+    IAuthLinkGenerator authLinkGenerator,
+    IAuthEmailSender emailSender)
 {
     private readonly IUserRepository _userRepository = userRepository;
     private readonly IPasswordHasher<User> _passwordHasher = passwordHasher;
+    private readonly IAuthTokenService _authTokenService = authTokenService;
+    private readonly IAuthLinkGenerator _authLinkGenerator = authLinkGenerator;
+    private readonly IAuthEmailSender _emailSender = emailSender;
 
     /// <summary>
     /// Registers a new user asynchronously using the specified registration details.
@@ -36,26 +54,145 @@ public class AuthServices(IUserRepository userRepository, IPasswordHasher<User> 
     /// found.</exception>
     public async Task<int> RegisterUserAsync(UserRegisterDto userRegisterDto, CancellationToken cancellationToken)
     {
-        ValidateUserRegisterDto(userRegisterDto);
-        ValidateEmailPatern(userRegisterDto.Email);
-        ValidatePasswordPatern(userRegisterDto.Password);
+        //Validate DTO
+        ValidateRegistrationInput(userRegisterDto);
 
-        if(await IsUsernameExists(userRegisterDto.Username, cancellationToken))
+        //Check if user exists
+        var normalizedUsername = NormalizeInput(userRegisterDto.Username);
+        var normalizedEmail = NormalizeInput(userRegisterDto.Email);
+        await EnsureUserDoesNotExists(normalizedUsername, normalizedEmail, cancellationToken);
+
+        //Create User
+        var user = CreateUserEntity(userRegisterDto, normalizedUsername, normalizedEmail);
+
+        //Assign Default Role
+        await AssignDefaultRoleAsync(user, cancellationToken);
+
+        //Save user
+        var userCreated = await _userRepository.AddUserAsync(user, cancellationToken);
+
+        //Create Confirmation Link
+        var confirmationLink = CreateEmailConfirmationLink(userCreated.Id);
+
+        //Send Confirmation Email
+        await _emailSender.SendEmailConfirmationAsync(userCreated.Email, confirmationLink, cancellationToken);
+
+        return userCreated.Id;
+    }
+
+    /// <summary>
+    /// Confirms a user's email address using an email confirmation token.
+    /// </summary>
+    /// <remarks>
+    /// This method validated the provided token for the <see cref="AuthTokenPurpose.EmailConfirmation"/> purpose, extracts the user identifier from 
+    /// the token payload, and marks the user account as email-confirmed.
+    /// If the user is already confirmed, the method returns without changes.
+    /// </remarks>
+    /// <param name="token">
+    /// The email confirmation token recieved from the confirmation link. Cannot be null or empty.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A cancellation token that can be used to cancel the operation.
+    /// </param>
+    /// <returns>
+    /// A task that represents the asynchronous confirmation operation.
+    /// </returns>
+    /// <exception cref="ArgumentException">
+    /// Thrown if <paramref name="token"/> is null or empty.
+    /// </exception>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown if the token is invalid, expired, or does not match the expected purpose.
+    /// </exception>
+    /// <exception cref="KeyNotFoundException">
+    /// Thrown if the user referenced by the token payload cannot be found.
+    /// </exception>
+    public async Task ConfirmEmailAsync(string token, CancellationToken cancellationToken)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(token, nameof(token));
+
+        if(!_authTokenService.TryValidateToken(token, AuthTokenPurpose.EmailConfirmation ,out var payload))
         {
-            throw new InvalidOperationException("A user with the provided username already exists.");
+            throw new InvalidOperationException("Invalid or expired email confirmation token.");
         }
 
-        if(await IsEmailExists(userRegisterDto.Email, cancellationToken))
+        var user = await _userRepository.GetUserByIdAsync(payload!.UserId, cancellationToken)
+            ?? throw new KeyNotFoundException("User not found.");
+
+        if (user.IsEmailConfirmed)
         {
-            throw new InvalidOperationException("A user with the provided email already exists.");
+            return;
         }
 
+        user.IsEmailConfirmed = true;
+        user.UpdatedAt = DateTime.UtcNow;
+        await _userRepository.UpdateUserAsync(user, cancellationToken);
+    }
+
+    #region of private methods
+    /// <summary>
+    /// Generates an email confirmation link for the specified user ID.
+    /// </summary>
+    /// <param name="userId">
+    /// The unique identifier of the user for whom to create the email confirmation link.
+    /// </param>
+    /// <returns>
+    /// The generated <see cref="Uri"/> representing the email confirmation link.
+    /// </returns>
+    private Uri CreateEmailConfirmationLink(int userId)
+    {
+        var token = _authTokenService.CreateToken(AuthTokenPurpose.EmailConfirmation, userId, DateTime.UtcNow.AddHours(24));
+        return _authLinkGenerator.CreateEmailConfirmationLink(token);
+    }
+
+    /// <summary>
+    /// Assigns the default role ("user") to the specified user.
+    /// </summary>
+    /// <param name="user">
+    /// The user entity to which the default role will be assigned.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// The cancellation token that can be used to cancel the operation.
+    /// </param>
+    /// <returns>
+    /// A task that represents the asynchronous operation.
+    /// </returns>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown if the default role "user" cannot be found in the repository.
+    /// </exception>
+    private async Task AssignDefaultRoleAsync(User user, CancellationToken cancellationToken)
+    {
+        var role = await _userRepository.GetRoleByNameAsync("user", cancellationToken)
+            ?? throw new InvalidOperationException("Default role 'user' not found.");
+
+        user.UserRoles.Add(new UserRole()
+        {
+            RoleId = role.Id,
+        });
+    }
+
+    /// <summary>
+    /// Creates a new User entity based on the provided registration data.
+    /// </summary>
+    /// <param name="dto">
+    /// The user registration data transfer object containing the user's details.
+    /// </param>
+    /// <param name="normalizedUsername">
+    /// The normalized username for the user.
+    /// </param>
+    /// <param name="normalizedEmail">
+    /// The normalized email address for the user.
+    /// </param>
+    /// <returns>
+    /// The newly created User entity, with hashed password and initialized properties.
+    /// </returns>
+    private User CreateUserEntity(UserRegisterDto dto, string normalizedUsername, string normalizedEmail)
+    {
         User user = new()
         {
-            UserName = userRegisterDto.Username,
-            NormalizedUsername = NormalizeInput(userRegisterDto.Username),
-            Email = userRegisterDto.Email,
-            NormalizedEmail = NormalizeInput(userRegisterDto.Email),
+            UserName = dto.Username,
+            NormalizedUsername = normalizedUsername,
+            Email = normalizedEmail,
+            NormalizedEmail = NormalizeInput(dto.Email),
             IsActive = true,
             IsDeleted = false,
             IsEmailConfirmed = false,
@@ -63,20 +200,22 @@ public class AuthServices(IUserRepository userRepository, IPasswordHasher<User> 
             UpdatedAt = DateTime.UtcNow,
             Tokens = []
         };
+        user.PasswordHash = PasswordHashing(dto.Password, user);
 
-        user.PasswordHash = PasswordHashing(userRegisterDto.Password, user);
+        return user;
+    }
 
-        var role = await _userRepository.GetRoleByNameAsync("user", cancellationToken) 
-            ?? throw new InvalidOperationException("Default role 'user' not found.");
-
-        user.UserRoles.Add(new UserRole()
-        {
-                RoleId = role.Id,
-        });
-
-        var userCreated = await _userRepository.AddUserAsync(user, cancellationToken);
-
-        return userCreated.Id;
+    /// <summary>
+    /// Validates the user registration input for required fields, email format, and password rules.
+    /// </summary>
+    /// <param name="dto">
+    /// The registration DTO to validate
+    /// </param>
+    private static void ValidateRegistrationInput(UserRegisterDto dto)
+    {
+        ValidateUserRegisterDto(dto);
+        ValidateEmailPatern(dto.Email);
+        ValidatePasswordPatern(dto.Password);
     }
 
     /// <summary>
@@ -134,33 +273,6 @@ public class AuthServices(IUserRepository userRepository, IPasswordHasher<User> 
     }
 
     /// <summary>
-    /// Determines whether a user with the specified username exists in the system.
-    /// </summary>
-    /// <param name="username">The username to check for existence. Cannot be null or empty.</param>
-    /// <param name="cancellationToken">A cancellation token that can be used to cancel the asynchronous operation.</param>
-    /// <returns>A task that represents the asynchronous operation. The task result is <see langword="true"/> if a user with the
-    /// specified username exists; otherwise, <see langword="false"/>.</returns>
-    private async Task<bool> IsUsernameExists (string username, CancellationToken cancellationToken)
-    {
-        var userName = await _userRepository.GetUserByNormalizedUsernameAsync(NormalizeInput(username), cancellationToken);
-        
-        return userName is not null;
-    }
-
-    /// <summary>
-    /// Determines whether a user account with the specified email address exists.
-    /// </summary>
-    /// <param name="email">The email address to check for existence. Cannot be null or empty.</param>
-    /// <param name="cancellationToken">A cancellation token that can be used to cancel the asynchronous operation.</param>
-    /// <returns>A task that represents the asynchronous operation. The task result is <see langword="true"/> if a user with the
-    /// specified email exists; otherwise, <see langword="false"/>.</returns>
-    private async Task<bool> IsEmailExists(string email, CancellationToken cancellationToken)
-    {
-        var userEmail = await _userRepository.GetUserByNormalizedEmailAsync(NormalizeInput(email), cancellationToken);
-        return userEmail is not null;
-    }
-
-    /// <summary>
     /// Converts the specified input string to its uppercase equivalent using the invariant culture.
     /// </summary>
     /// <param name="input">The string to normalize. If null, a NullReferenceException will be thrown.</param>
@@ -182,4 +294,42 @@ public class AuthServices(IUserRepository userRepository, IPasswordHasher<User> 
         }
         return _passwordHasher.HashPassword(user, password);
     }
+
+    /// <summary>
+    /// Ensures that no user exists with the specified normalized username or normalized email.
+    /// </summary>
+    /// <remarks>
+    /// This method checks the user repository for existing users with the given normalized username and email. 
+    /// If a user is found with either identifier, an <see cref="InvalidOperationException"/> is thrown to indicate that the user 
+    /// cannot be created due to existing records.
+    /// </remarks>
+    /// <param name="normalizedUsername">
+    /// The normalized username to check for existing users.
+    /// </param>
+    /// <param name="normalizedEmail">
+    /// The normalized email to check for existing users.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// The cancellation token that can be used to cancel the operation.
+    /// </param>
+    /// <returns>
+    /// The task representing the asynchronous operation.
+    /// </returns>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown if a user with the provided username or email already exists.
+    /// </exception>
+    private async Task EnsureUserDoesNotExists(string normalizedUsername, string normalizedEmail, CancellationToken cancellationToken)
+    {
+        if (await _userRepository.GetUserByNormalizedUsernameAsync(normalizedUsername, cancellationToken) is not null)
+        {
+            throw new InvalidOperationException("A user with the provided username already exists.");
+        }
+        
+        if (await _userRepository.GetUserByNormalizedEmailAsync(normalizedEmail, cancellationToken) is not null)
+        {
+            throw new InvalidOperationException("A user with the provided email already exists.");
+        }
+    }
+
+    #endregion of private methods
 }
