145b3370da65ba087b03237d1a3e07162f9eb313
marinadotnet <msichova@outlook.com>
2026-02-09 12:17:20 +0800

    Create IRefreshTokenGenerator.cs
    Add IRefreshTokenGenerator interface

---
+++ BookShop.API/Services/IRefreshTokenGenerator.cs
@@ -0,0 +1,15 @@
+﻿namespace BookShop.API.Services;
+
+/// <summary>
+/// Generates crypthography secure refresh tokens.
+/// </summary>
+public interface IRefreshTokenGenerator
+{
+    /// <summary>
+    /// Generates a new refresh token.
+    /// </summary>
+    /// <returns>
+    /// A URL-safe refresh token string.
+    /// </returns>
+    string GenerateRefreshToken();
+}
