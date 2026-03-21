1c228f737a71c84cf92909d7627a67e1c0979428
Marina Sichova <msichova@outlook.com>
2026-03-21 15:10:37 +0800

    Add current suer endpoint to AuthController

--- BookShop.API/Controllers/V1/AuthController.cs
+++ BookShop.API/Controllers/V1/AuthController.cs
@@ -576,4 +576,34 @@ public class AuthController(AuthServices auth) : BaseApiController
         """;
         return Content(html, "text/html");
     }
+
+    /// <summary>
+    /// Retrieves the profile of the currently authenticated user.
+    /// </summary>
+    /// <param name="cancellationToken">
+    /// A token that can be used to cancell the operation.
+    /// </param>
+    /// <returns>
+    /// <see cref="OkObjectResult"/> containing the current user's profile information. 
+    /// </returns>
+    /// <response code="200">
+    /// Returns the user profile.
+    /// </response>
+    /// <response code="401">
+    /// The user is not authenticated.
+    /// </response>
+    /// <response code="403">
+    /// The user is not allowed to access this resource.
+    /// </response>
+    [HttpGet("account")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<IActionResult> GetCurrentUser(CancellationToken cancellationToken)
+    {
+        var userId = GetCurrentUserId();
+        var user = await _auth.GetCurrentUserAsync(userId, cancellationToken);
+        return Ok(user);
+    }
 }
