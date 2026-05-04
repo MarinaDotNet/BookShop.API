3568678057c94cd3b457eee035cd6a110914656a
Marina Sichova <msichova@outlook.com>
2026-05-04 17:09:46 +0800

    Add V2 endpoints: GetById and GetByExactMatch

--- BookShop.API/Controllers/V2/BooksController.cs
+++ BookShop.API/Controllers/V2/BooksController.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Asp.Versioning;
 using BookShop.API.DTOs.Catalog;
 using BookShop.API.Services;
@@ -56,4 +57,97 @@ public class BooksController(IBookService service) : ControllerBase
     {
         return Ok( await _service.GetAllBooksAsync(true));
     }
+
+    /// <summary>
+    /// Retrieves a book by its unique identifier. This endpoint is mapped to API version 2.0 and returns the book if it is found
+    /// and currently available. If the book is not found or is not available, a 404 Not Found response is returned.
+    /// </summary>
+    /// <param name="id">
+    /// The unique identifier of the book to retrieve.
+    /// </param>
+    /// <returns>
+    /// The requested book if found and available. Returns HTTP 200 OK.
+    /// If the book is not found or is not available, a 404 Not Found response is returned.
+    /// </returns>
+    /// <exception cref="KeyNotFoundException">
+    /// Thrown when a book with the specified <paramref name="id" /> is not found or is not currently available.
+    /// </exception>
+    /// <response code="200">
+    /// The requested book is returned successfully.
+    /// </response>
+    /// <response code="400">
+    /// The provided identifier is invalid (e.g., null, empty, or not a valid ObjectId).
+    /// </response>
+    ///  <response code="401">
+    /// The request is unauthorized.
+    /// This can occur when:
+    /// - The user is not authenticated (no valid token provided)
+    /// - The token has expired
+    /// - The token is invalid or malformed
+    /// </response>
+    /// <response code="403">
+    /// The request is forbidden.
+    /// This can occur when:
+    /// - The user is authenticated but does not have the required role to access this endpoint.
+    /// </response>
+    /// <response code="404">
+    /// A book with the specified identifier is not found or is not currently available.
+    /// </response>
+    [HttpGet("{id}")]
+    [MapToApiVersion("2.0")]
+    [ProducesResponseType(typeof(BookDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetById(string id)
+    {
+        var book = await _service.GetBookByIdAsync(id);
+
+        if (book is null || !book.IsAvailable)
+        {
+            throw new KeyNotFoundException("Book not found.");
+        }
+        return Ok(book);
+    }
+    
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="request">
+    /// The search criteria encapsulated in a <see cref="BookSearchRequestDto"/> object.
+    /// </param>
+    /// <returns>
+    /// An <see cref="IActionResult"/> containing the list of books that exactly match the search criteria.
+    /// </returns>
+    /// <response code="200">
+    /// The search was successful, and a collection of books that exactly match the specified criteria is returned.
+    /// If no books match the criteria, an empty collection is returned with HTTP 200 status code.
+    /// </response>
+    /// <response code="400">
+    /// The search request is invalid. This can occur when:
+    /// - The search term is null, empty, or contains only whitespace.
+    /// </response>
+    /// <response code="401">
+    /// The request is unauthorized. This can occur when:
+    /// - The user is not authenticated (no valid token provided)
+    /// - The token has expired
+    /// - The token is invalid or malformed
+    /// </response>
+    /// <response code="403">
+    /// The request is forbidden. This can occur when:
+    /// - The user is authenticated but does not have the required "admin" role to access this endpoint.
+    /// </response>
+    [HttpGet("search-exact")]
+    [MapToApiVersion("2.0")]
+    [ProducesResponseType(typeof(IReadOnlyCollection<BookDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<IActionResult> GetByExactMatch([FromQuery] BookSearchRequestDto request)
+    {
+        return Ok(await _service.GetAvailableBooksByExactMatchAsync(request));
+        
+    }
+
 }
