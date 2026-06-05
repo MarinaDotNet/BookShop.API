11db895d3a17708ccf0b86c7bf654acfe83d5fb6
Marina Sichova <msichova@outlook.com>
2026-06-05 10:54:53 +0800

    Add docs for ForgotPasswordValidator

--- BookShop.API/Validators/ForgotPasswordValidator.cs
+++ BookShop.API/Validators/ForgotPasswordValidator.cs
@@ -4,8 +4,15 @@ using BookShop.API.Helpers;
 
 namespace BookShop.API.Validators;
 
+/// <summary>
+/// Validator for the <see cref="ForgotPasswordDto"/> record, ensuring that the email provided is valid and properly formatted. 
+/// </summary>
 public sealed class ForgotPasswordValidator : AbstractValidator<ForgotPasswordDto>
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ForgotPasswordValidator"/> class and defines the validation rules for the <see cref="ForgotPasswordDto"/>
+    /// object. The validator checks that the email is not null, empty, or whitepace, and that it follows a valid email format.  
+    /// </summary>
     public ForgotPasswordValidator()
     {
         RuleFor(x => x.Email)
