7d8151f535a639988e94b16364ac76edf9be4bf5
Marina Sichova <msichova@outlook.com>
2026-06-20 18:54:18 +0800

    Add OrdersController.V2 with GetById endpoint

---
+++ BookShop.API/Controllers/V2/OrdersController.cs
@@ -0,0 +1,56 @@
+using BookShop.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using BookShop.API.DTOs.Order;
+
+namespace BookShop.API.Controllers.V2;
+
+/// <summary>
+/// Manages order retrieval and status opeations for authenticated users.
+/// </summary>
+[ApiController]
+[EnableCors("PublicPolicy")]
+[Authorize(Roles = "user, admin")]
+[ApiVersion("2.0")]
+[Route("api/v{version:apiVersion}/[controller]")]
+public class OrdersController(IOrderService service) : BaseApiController
+{
+    private readonly IOrderService _service = service;
+
+    /// <summary>
+    /// Retrieves an order by its unique identifier.
+    /// </summary>
+    /// <param name="orderId">
+    /// The unique identifier of the order.
+    /// </param>
+    /// <returns>
+    /// The order is found.
+    /// </returns>
+    /// <response code="200">
+    /// Returns the order.
+    /// </response>
+    /// <response code="400">
+    /// The provided order ID is invalid.
+    /// </response>
+    /// <response code="401">
+    /// The user is not authenticated.
+    /// </response>
+    /// <response code="404">
+    /// No order with the specified ID was found.
+    /// </response>
+    [HttpGet("{orderId}")]
+    [MapToApiVersion("2.0")]
+    [ProducesResponseType(typeof(OrderDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> GetById([FromRoute]int orderId)
+    {
+        var result = await _service.GetByIdAsync(orderId);
+        return result == null 
+            ? NotFound() 
+            : Ok(result);
+    }
+}
