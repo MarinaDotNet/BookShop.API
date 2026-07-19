5bd432f4bb38ba2d9bc72fe91809eb1460f8fb8e
Marina Sichova <msichova@outlook.com>
2026-07-19 12:35:31 +0800

    refactor: change UserLoginDto from record to class

--- BookShop.Web/DTOs/Auth/UserLoginDto.cs
+++ BookShop.Web/DTOs/Auth/UserLoginDto.cs
@@ -1,12 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BookShop.Web.DTOs.Auth;
 
 /// <summary>
 /// Represents the credentials required to authenticate a user.
 /// </summary>
-/// <param name="Email">
-/// The user's email.
-/// </param>
-/// <param name="Password">
-/// The account password.
-/// </param>
-public sealed record UserLoginDto(string Email, string Password);
+public sealed class UserLoginDto
+{
+    /// <summary>
+    /// User email.
+    /// </summary>
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    /// <summary>
+    /// User password.
+    /// </summary>
+    [Required]
+    public string Password { get; set; } = string.Empty;
+}
